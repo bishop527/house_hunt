b5191f621bd9fd7e45b66cb2f1a0f61fe1fb9c37
Adrian <aedwards92@gmail.com>
2015-11-10 21:05:30 -0500

    .

--- src/utils.py
+++ src/utils.py
@@ -5,10 +5,27 @@ Created on Nov 4, 2015
 '''
 import pandas as pd
 import platform
-import os
 import pwd
 import urllib2
+import openpyxl as pyxl
+import os.path
 
+""" 
+Appends the given DataFrame with the master workbook and names the worksheet the given sheetName 
+"""
+def populateMaster(fileName, df):
+    for sheetName, data in df.iteritems():
+        print "Adding", sheetName, "to", fileName
+        if os.path.isfile(fileName):
+            book = pyxl.load_workbook(fileName)
+            writer = pd.ExcelWriter(fileName, engine='openpyxl')
+            writer.book = book
+            writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+        else:
+            writer = pd.ExcelWriter(fileName, engine='openpyxl')    
+        data.to_excel(writer, sheetName)
+        writer.save()
+    
 """
 Exporting data from the profiles.doe.mass.edu site does not save data as
 true Excel files. This mehtod will save the data as an xls file so it 
@@ -36,4 +53,26 @@ def setCurrDir():
     elif platform.system() == "Windows":
         os.chdir("c:\\Users\\"+user+"\\workspace\\house_hunt\\")
         print "Changed directory to c:\\Users\\"+user+"\\workspace\\house_hunt\\"
+
+def getMATowns():
+    towns = ['Abington','Acton','Acushnet','Adams','Agawam','Alford','Amesbury','Amherst','Andover','Aquinnah','Arlington','Ashburnham','Ashby','Ashfield','Ashland','Athol','Attleboro','Auburn','Avon','Ayer',
+    'Barnstable','Barre','Becket','Bedford','Belchertown','Bellingham','Belmont','Berkley','Berlin','Bernardston','Beverly','Billerica','Blackstone','Blandford','Bolton','Boston','Bourne','Boxborough','Boxford','Boylston','Braintree','Brewster','Bridgewater','Brighton','Brimfield','Brockton','Brookfield','Brookline','Buckland','Burlington',
+    'Cambridge','Canton','Carlisle','Carver','Charlemont','Charlestown','Charlton','Chatham','Chelmsford','Chelsea','Cheshire','Chester','Chesterfield','Chicopee','Chilmark','Clarksburg','Clinton','Cohasset','Colrain','Concord','Conway','Cummington',
+    'Dalton','Dana','Danvers','Dartmouth','Dedham','Deerfield','Dennis','Dighton','Douglas','Dorchester','Dover','Dracut','Dudley','Dunstable','Duxbury',
+    'East Bridgewater','East Brookfield','East Longmeadow','Eastham','Easthampton','Easton','Edgartown','Egremont','Enfield','Erving','Essex','Everett',
+    'Fairhaven','Fall River','Falmouth','Fitchburg','Florida','Foxborough','Framingham','Franklin','Freetown',
+    'Gardner','Georgetown','Gill','Gloucester','Goshen','Gosnold','Grafton','Granby','Granville','Great Barrington','Greenfield','Greenwich','Groton','Groveland',
+    'Hadley','Halifax','Hamilton','Hampden','Hancock','Hanover','Hanson','Hardwick','Harvard','Harwich','Hatfield','Haverhill','Hawley','Heath','Hingham','Hinsdale','Holbrook','Holden','Holland','Holliston','Holyoke','Hopedale','Hopkinton','Hubbardston','Hudson','Hull','Huntington','Hyde Park',
+    'Ipswich','Kingston',
+    'Lakeville','Lancaster','Lanesborough','Lawrence','Lee','Leicester','Lenox','Leominster','Leverett','Lexington','Leyden','Lincoln','Littleton','Longmeadow','Lowell','Ludlow','Lunenburg','Lynn','Lynnfield',
+    'Malden','Manchester-By-The-Sea','Mansfield','Marblehead','Marion','Marlborough','Marshfield','Mashpee','Mattapoisett','Maynard','Medfield','Medford','Medway','Melrose','Mendon','Merrimac','Methuen','Middleborough','Middlefield','Middleton','Milford','Millbury','Millis','Millville','Milton','Monroe','Monson','Montague','Monterey','Montgomery','Mount Washington',
+    'Nahant','Nantucket','Natick','Needham','New Ashford','New Bedford','New Braintree','New Marlborough','New Salem','Newbury','Newburyport','Newton','Norfolk','North Adams','North Andover','North Attleborough','North Brookfield','North Reading','Northampton','Northborough','Northbridge','Northfield','Norton','Norwell','Norwood',
+    'Oak Bluffs','Oakham','Orange','Orleans','Otis','Oxford',
+    'Palmer','Paxton','Peabody','Pelham','Pembroke','Pepperell','Peru','Petersham','Phillipston','Pittsfield','Plainfield','Plainville','Plymouth','Plympton','Prescott','Princeton','Provincetown',
+    'Quincy','Randolph','Raynham','Reading','Rehoboth','Revere','Richmond','Rochester','Rockland','Rockport','Rowe','Rowley','Roxbury','Royalston','Russell','Rutland',
+    'Salem','Salisbury','Sandisfield','Sandwich','Saugus','Savoy','Scituate','Seekonk','Sharon','Sheffield','Shelburne','Sherborn','Shirley','Shrewsbury','Shutesbury','Somerset','Somerville','South Hadley','Southampton','Southborough','Southbridge','Southwick','Spencer','Springfield','Sterling','Stockbridge','Stoneham','Stoughton','Stow','Sturbridge','Sudbury','Sunderland','Sutton','Swampscott','Swansea',
+    'Taunton','Templeton','Tewksbury','Tisbury','Tolland','Topsfield','Townsend','Truro','Tyngsborough','Tyringham',
+    'Upton','Uxbridge','Wakefield','Wales','Walpole','Waltham','Ware','Wareham','Warren','Warwick','Washington','Watertown','Wayland','Webster','Wellesley','Wellfleet','Wendell','Wenham','West Boylston','West Bridgewater','West Brookfield','West Newbury','West Roxbury','West Springfield','West Stockbridge','West Tisbury','Westborough','Westfield','Westford','Westhampton','Westminster','Weston','Westport','Westwood','Weymouth','Whately','Whitman','Wilbraham','Williamsburg','Williamstown','Wilmington','Winchendon','Winchester','Windsor','Winthrop','Woburn','Worcester','Worthington','Wrentham','Yarmouth']
+    
+    return towns
         
