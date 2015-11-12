9c50751a2bfba09b640e57cc8575deb6b87200cf
Adrian <aedwards92@gmail.com>
2015-11-12 11:17:07 -0500

    Beginnings of Town Data

--- src/Town/ParseTownData.py
+++ src/Town/ParseTownData.py
@@ -3,7 +3,6 @@ Created on Nov 10, 2015
 
 @author: ad23883
 '''
-import json
 import pandas as pd
 import utils
 
@@ -11,14 +10,13 @@ dataLocation = "data/town/"
 ext = ".xlsx"
 
 def parseCommuteData(data):
-    #data = json.load(data)
     columns = ['Town', 'Distance to Work', 'Min to Work']
     rows = []
     
     for row in range(len(data["rows"])):
         origin = str(data['origin_addresses'][row]).split(',')[0]
         destination = data['destination_addresses'][0]
-        duration = str(data['rows'][row]['elements'][0]['duration']['text']).split(' ')[0]
+        duration = utils.convertToMin(str(data['rows'][row]['elements'][0]['duration']['text']))
         distance = str(data['rows'][row]['elements'][0]['distance']['text']).split(' ')[0]
         rows.append([origin, distance, duration])
    
