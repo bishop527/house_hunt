b1ba54558cfb27e781426b34b3ea015bd84c8079
Adrian <aedwards92@gmail.com>
2015-11-12 11:17:07 -0500

    Beginnings of Town Data

--- src/ProcessTownData.py
+++ src/ProcessTownData.py
@@ -3,7 +3,10 @@ Created on Nov 10, 2015
 
 @author: ad23883
 '''
-from utils import *
+from utils import setCurrDir
+from utils import setProxy
+from utils import populateMaster
+
 from Town.CommuteData import getCommuateData
 from Town.ParseTownData import parseCommuteData
 from collections import OrderedDict
@@ -12,9 +15,12 @@ fileName = "house_hunting-Town_Data-2015.xlsx"
 entries = OrderedDict()
 
 setCurrDir()
-#utils.setProxy()
-commuteData = getCommuateData()
-entries['Commute'] = parseCommuteData(commuteData)
+setProxy()
+commuteData1, commuteData2 = getCommuateData()
+df1 = parseCommuteData(commuteData1)
+df2 = parseCommuteData(commuteData2)
+df1.append(df2)
+entries['Commute'] = df1
 
 populateMaster(fileName, entries)
 
--- src/Town/CommuteData.py
+++ src/Town/CommuteData.py
@@ -8,8 +8,7 @@ import googlemaps
 
 dataLocation = 'data/town/'
 
-def getCommuateData():
-    
+def getCommuateData():    
     googleAPIkey = 'AIzaSyDtNP2h8YzQzUdWJ_2JvspP4nAJhg7m9LQ'
     gmaps = googlemaps.Client(key=googleAPIkey)
     destination = '244 Wood St. Lexington, MA'
@@ -22,14 +21,17 @@ def getCommuateData():
     origins = ''
     
     """ Need to seperate list of towns into chunks of 100 """
-    for each in towns[:105]:
+    for each in towns[:25]:
         origins += each+', MA|'
     origins = origins[:-1]
     
-    commuteData = gmaps.distance_matrix(origins, destination, mode=mode, language=language, avoid=avoid, units=units)
-#     origins = ''
-#     for each in towns[:100]:
-#         origins += each+', MA|'
-#     origins = origins[:-1]
+    commuteData1 = gmaps.distance_matrix(origins, destination, mode=mode, language=language, avoid=avoid, units=units)
     
-    return commuteData
+    origins = ''
+    for each in towns[25:50]:
+        origins += each+', MA|'
+    origins = origins[:-1]
+    
+    commuteData2 = gmaps.distance_matrix(origins, destination, mode=mode, language=language, avoid=avoid, units=units)
+    
+    return commuteData1, commuteData2
